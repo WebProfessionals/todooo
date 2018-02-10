bdfab74f5c50d0e372a11d7d6ba9d4cfa275a4ff
veith <veithz@gmail.com>
2018-02-10 09:43:37 +0100

    init Testumgebung

---
+++ todo.js
@@ -0,0 +1,15 @@
+"use strict"
+
+class Task{
+
+}
+
+class TodoList{
+
+}
+
+
+if ( typeof module !== 'undefined' && module ){
+  module.exports.Task = Task;
+  module.exports.TodoList = TodoList;
+}
--- todo_test.js
+++ todo_test.js
@@ -1,11 +1,15 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
 
+var Task = require('./todo.js').Task;
+var TodoList = require('./todo.js').TodoList;
 
 describe('Todo', function () {
   describe('Task', function () {
 
+    it('sollte 1+1 addieren können', function () {
 
+    });
 
   });
 });
