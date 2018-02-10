0a39e4781d5e775306c251a24662f02134841619
veith <veithz@gmail.com>
2018-02-10 10:03:03 +0100

    erste Tests

--- todo.js
+++ todo.js
@@ -1,15 +1,25 @@
 "use strict"
 
-class Task{
+class Task {
+  constructor() {
+    this.id = 1;
+    this.text = '';
+    this.erledigt = false;
+    this.position = 0;
+
+  }
 
 }
 
-class TodoList{
+class TodoList {
+  constructor() {
+    this.tasks = [];
+  }
 
 }
 
 
-if ( typeof module !== 'undefined' && module ){
+if (typeof module !== 'undefined' && module) {
   module.exports.Task = Task;
   module.exports.TodoList = TodoList;
 }
--- todo_test.js
+++ todo_test.js
@@ -7,8 +7,27 @@ var TodoList = require('./todo.js').TodoList;
 describe('Todo', function () {
   describe('Task', function () {
 
-    it('sollte 1+1 addieren können', function () {
+    it('wir sollten ein Task Objekt erzeugen können', function () {
+      let t = new Task;
+      assert.instanceOf(t, Task);
+    });
 
+    it('wir sollten ein TodoList Objekt erzeugen können', function () {
+      let t = new TodoList;
+      assert.instanceOf(t, TodoList);
+    });
+
+    it('Todolist sollte ein Attribut tasks (array) mit Tasks haben', function () {
+      let tl = new TodoList;
+      assert.typeOf(tl.tasks, 'array');
+    });
+
+    it('Tasks sollten id,text,erledigt,position haben', function () {
+      let t = new Task;
+      expect(t.id).to.not.equal(undefined);
+      expect(t.text).to.not.equal(undefined);
+      expect(t.erledigt).to.not.equal(undefined);
+      expect(t.position).to.not.equal(undefined);
     });
 
   });
